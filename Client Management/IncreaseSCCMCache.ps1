b5d6e5a0551a842eb568dd6ee4591370a040c28f
sccmtst <36771799+sccmtst@users.noreply.github.com>
2018-04-27 09:13:50 -0600

    Set SCCM Client Cache
    Changes to start and stop the service

--- Client Management/IncreaseSCCMCache.ps1
+++ Client Management/IncreaseSCCMCache.ps1
@@ -1,4 +1,12 @@
+
+try {
+    Stop-Service -Name CcmExec -ErrorAction Stop
+}
+catch {
+    Write-Error "Could not stop CcmExec Service: $_"
+}
+
 $Cache = Get-WmiObject -Namespace 'ROOT\CCM\SoftMgmtAgent' -Class CacheConfig
 $Cache.Size = '5000'
 $Cache.Put()
-Restart-Service -Name CcmExec
+Start-Service -Name CcmExec
